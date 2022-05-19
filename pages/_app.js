44ac05f44493062c0ce8a18cac5702ddfefb06df
SaburovBabur <saburovbabur@gmail.com>
2022-05-19 17:59:31 +0500

    Fix font mismatch

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "styles/tailwind.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="h-screen w-screen">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <div className="h-screen w-screen">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 
