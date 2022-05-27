4a3f22e7ee9f3b224426030282f8b1c65ed004da
SaburovBabur <saburovbabur@gmail.com>
2022-05-27 07:57:10 +0500

    Complete Sections UI /

---
+++ #/icons/SvgRightArrow.jsx
@@ -0,0 +1,17 @@
+function SvgRightArrow(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 48 26"
+      {...props}
+    >
+      <path
+        fill="#F37D26"
+        d="M36.742 6.103a1 1 0 10-1.414 1.415l1.414-1.415zM42.224 13l.707.707a1 1 0 000-1.415l-.707.708zm-6.896 5.482a1 1 0 001.414 1.414l-1.414-1.414zM6 12a1 1 0 100 2v-2zm36.206 2a1 1 0 000-2v2zm-6.878-6.482l6.19 6.189 1.413-1.415-6.189-6.189-1.414 1.415zm6.19 4.775l-6.19 6.189 1.414 1.414 6.19-6.19-1.415-1.413zM6 14h36.206v-2H6v2z"
+      ></path>
+    </svg>
+  );
+}
+
+export default SvgRightArrow;
